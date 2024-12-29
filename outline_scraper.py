3b8acd898d8c1822da7fe5f97326ff8d29f098aa
pjonany <philipjonany@gmail.com>
2024-12-29 01:13:28 -0800

    I'm actually goated, figured out splitting chapter s and verses

--- outline_scraper.py
+++ outline_scraper.py
@@ -1,10 +1,11 @@
 # outline-scraper.py
 
 import re
+f = open("message2.txt", "r") 
 
 BOOK_ABBREVIATIONS = [
     "Gen.", "Exo.", "Lev.", "Num.", "Deut.", "Josh.", "Judg.", "Ruth", "1 Sam.", "2 Sam.", 
-    "1 Kings", "2 Kings", "1 Chron.", "2 Chron.", "Ezra", "Neh.", "Esth.", "Job", "Ps.", "Prov.",
+    "1 Kings", "2 Kings", "1 Chron.", "2 Chron.", "Ezra", "Neh.", "Esth.", "Job", "Psa.", "Prov.",
     "Eccl.", "Song", "Isa.", "Jer.", "Lam.", "Ezek.", "Dan.", "Hos.", "Joel", "Amos", "Obad.", 
     "Jonah", "Mic.", "Nah.", "Hab.", "Zeph.", "Hag.", "Zech.", "Mal.", "Matt.", "Mark", "Luke", 
     "John", "Acts", "Rom.", "1 Cor.", "2 Cor.", "Gal.", "Eph.", "Phil.", "Col.", "1 Thess.", 
@@ -12,13 +13,14 @@ BOOK_ABBREVIATIONS = [
     "2 John", "3 John", "Jude", "Rev."
 ]
 
-f = open("message2.txt", "r") 
-
+# returns list of tuples (book, text_between_books)
 def split_by_books(text):
-    pattern = r'(' + '|'.join(re.escape(book) for book in BOOK_ABBREVIATIONS) + r')'
+    # Sort books so 1 John is deteced before John
+    pattern = r"(" + "|".join(map(re.escape, sorted(BOOK_ABBREVIATIONS))) + r")"
+    print(pattern)
     sections = re.split(pattern, text)
     result = []
-    # Starts with message title before any verses
+    # Starts with message title before any verse refs, so skip 0
     i = 1
     while i < len(sections):
         book = sections[i]
@@ -28,18 +30,53 @@ def split_by_books(text):
         i += 2
     return result
 
+# Takes one section between books, and splits it by chapters
+def split_by_chapters(text):
+    pattern = r"(\d+:\d+)"
+
+    # split into alternating between chapter and section between chapter
+    sections = re.split(pattern, text)
+    result = []
+
+    # Find first chapter ref
+    i = 0
+    while i < len(sections):
+        section = sections[i]
+        if re.match(r"\d+", section):
+            break
+        i += 1
+    if i >= len(sections): 
+        print("UNEXPECTED ERROR: NO CHAPTERS FOUND IN " + text)
+
+    while i < len(sections):
+        section = sections[i]
+        colonIndex = section.find(":")
+        section_after_chapter = sections[i + 1] if (i + 1) < len(sections) else ""
+        result.append((section[:colonIndex], section[colonIndex:] + section_after_chapter))
+        i += 2
+    return result
+
+# Finds all number-number patterns and single numbers, return list of integers.
+def getAllVerseNumbers(s):
+    pattern = r"(\d+-\d+)"
+
 def runAndPrint(s):
     result = split_by_books(s)
     for book, section in result:
         print("-----")
-        print(book + "\n" + section)
-
+        print(book)
+        verses = split_by_chapters(section)
+        for chapter, section in verses:
+            print("\t" + chapter)
+            print("\t\t" + section)
 
 # main
-s = f.read()
-runAndPrint(s)
-f.close()
-
+def main():
+    s = f.read()
+    runAndPrint(s)
+    f.close()
+if __name__ == "__main__":
+    main()
 '''
 Edge cases:
 1) Matt. 12:26
--- tests.py
+++ tests.py
@@ -1,12 +1,15 @@
 import unittest
 
 from outline_scraper import split_by_books
-test1 = "in the millennial kingdom)-Num. 14:27-30; Phil. 3:12-14: Ten of the twelve men"
-test1_answer = [("Num.", " 14:27-30; "), ("Phil.", " 3:12-14: Ten of the twelve men")]
 
 class TestSplitByBooks(unittest.TestCase):
-    def test_split_by_books(self):
-        self.assertEqual(test1_answer, split_by_books(test1))
+    def test_equals(self):
+        # split_by_book tests
+        split_by_books_1 = "in the millennial kingdom)-Num. 14:27-30; Phil. 3:12-14: Ten of the twelve men"
+        split_by_books_1_answer = [("Num.", " 14:27-30; "), ("Phil.", " 3:12-14: Ten of the twelve men")]
+        self.assertEqual(split_by_books_1_answer, split_by_books(split_by_books_1))
+
+        # split_section_by_chapters tests
 
 if __name__ == '__main__':
     unittest.main()
